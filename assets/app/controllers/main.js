b0574f0d4ed480986ffc6994feb9fb617034d0ad
Jesse Bartola <jrbartola@gmail.com>
2016-05-10 00:52:24 -0400

    Added waypoints and changed login/register animations/panels

--- assets/app/controllers/main.js
+++ assets/app/controllers/main.js
@@ -14,17 +14,56 @@ main.factory('loginFactory', function($http) {
 });
 
 main.controller('MainCtrl', function($scope, loginFactory) {
-	// Scope view of 0 is the initial wave cal/login/register box
-	// Scope view of 1 is the login loading animation
-	// Scope view of 2 is the registration form
+	// Scope view of 0 is the description of wave calendar
+	// Scope view of 1 is the login form
+	// Scope view of 2 is the registration form; leads to dashboard
 	// Scope view of 3 is the completed registration form; leads to user profile
 	$scope.view = 0;
 	$scope.email = '';
 	$scope.pass = '';
 
-	$scope.registration = function() {
+  $scope.changeView = function(view) {
 
-	}
+  }
+
+  $scope.startLogin = function() {
+    $.scrollTo($('#signup'), 800, {
+      onAfter: function() {
+        if ($scope.view != 1) {
+          $("#bannertitles").find(':not(.ng-hide)').addClass('animated bounceOutLeft');
+          $(".description").addClass('animated bounceOutLeft');
+          $('#bannertitles').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $scope.$apply(function() {
+
+              $scope.view = 1;
+              $($("#bannertitles").children()[1]).addClass('animated bounceInRight');
+              $(".signin").addClass('animated bounceInRight');
+
+            });
+          });
+        }  
+      }
+    });
+
+  }
+
+  $scope.startRegister = function() {
+    $.scrollTo($('#signup'), 800, {
+      onAfter: function() {
+        if ($scope.view != 2) {
+          $("#bannertitles").find(':not(.ng-hide)').addClass('animated bounceOutLeft');
+          $('#bannertitles').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $scope.$apply(function() {
+              $scope.view = 2;
+              $($("#bannertitles").children()[2]).addClass('animated bounceInRight');
+            });
+          });
+        }
+      }
+    });
+    
+  }
+
 });
 
 // Directive for login/registration panel
@@ -33,18 +72,40 @@ main.directive('frontwave', function($timeout) {
   return {
     restrict: 'AEC',
     scope: {
-      efield: '=',
-      pfield: '=',
+      onLogin: '&',
       onRegister: '&'
 
     },
     link: function(scope, elem, attrs) {
       // insert scope functions here
       // scope.function()...
-      scope.getemail = function() {
-      	console.log(scope.efield);
-      };
+      // scope.getemail = function() {
+      // 	//console.log(scope.efield);
+      // };
     },
     templateUrl: '../templates/frontwave.html'
+  };
+});
+
+// Directive for login/registration panel
+main.directive('bottombanner', function($timeout) {
+  
+  return {
+    restrict: 'AEC',
+    scope: {
+      efield: '=',
+      pfield: '=',
+      onRegister: '&',
+      view: '='
+
+    },
+    link: function(scope, elem, attrs) {
+      // insert scope functions here
+      // scope.function()...
+      scope.getemail = function() {
+        console.log(scope.efield);
+      };
+    },
+    templateUrl: '../templates/bottombanner.html'
   };
 });
